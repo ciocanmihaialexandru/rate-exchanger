d6ae53744c07235f111502f81c91c8b9454954ed
alexandru.ciocan <ciocanmihaialexandru@yahoo.com>
2022-01-13 01:13:14 +0200

    Added greenfield project

---
+++ src/main/java/com/rate/exchanger/RateExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.rate.exchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateExchangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rate/exchanger/RateExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rate.exchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateExchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
