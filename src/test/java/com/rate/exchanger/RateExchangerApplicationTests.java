b4bbcde4d47a5e24636c338755fee45305e6468c
alexandru.ciocan <ciocanmihaialexandru@yahoo.com>
2022-01-14 03:32:43 +0200

    Fixed test.

--- src/test/java/com/rate/exchanger/RateExchangerApplicationTests.java
+++ src/test/java/com/rate/exchanger/RateExchangerApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
@@ -21,7 +20,7 @@ class RateExchangerApplicationTests {
 	@Autowired
 	AccountsService accountsService;
 
-	@MockBean
+	@Autowired
 	private BankAccountRepository bankAccountRepository;
 
 	@Test
