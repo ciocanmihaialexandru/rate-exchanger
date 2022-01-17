6ae4d9ac9471c38de877419d57f3417b46a8a495
alexandru.ciocan <ciocanmihaialexandru@yahoo.com>
2022-01-17 23:58:41 +0200

    Added logs when dealing with exceptions.

--- src/main/java/com/rate/exchanger/exception/ExceptionHandlerManager.java
+++ src/main/java/com/rate/exchanger/exception/ExceptionHandlerManager.java
@@ -1,6 +1,8 @@
 package com.rate.exchanger.exception;
 
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,9 +16,12 @@ import java.util.Map;
 @ControllerAdvice
 public class ExceptionHandlerManager extends ResponseEntityExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ExceptionHandlerManager.class);
+
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<Object> handleBankAccountNotFoundException(ObjectNotFoundException ex) {
 
+        log.error("handleBankAccountNotFoundException(): Object not found", ex);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
@@ -27,6 +32,7 @@ public class ExceptionHandlerManager extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CallNotPermittedException.class)
     public ResponseEntity<Object> handleCircuitBreakerException(CallNotPermittedException ex) {
 
+        log.error("handleCircuitBreakerException(): Circuit breaker exception", ex);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
@@ -37,6 +43,7 @@ public class ExceptionHandlerManager extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleRuntimeException(Exception ex) {
 
+        log.error("handleRuntimeException(): Runtime exception", ex);
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
